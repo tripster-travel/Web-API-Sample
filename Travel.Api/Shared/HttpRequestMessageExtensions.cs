0236217f70a3df38b0607bc5216f2ae498030b6d
Fred <fcsobel@Yahoo.com>
2019-05-01 20:17:45 -0500

    move to auth folder

--- Travel.Api/Shared/HttpRequestMessageExtensions.cs
+++ Travel.Api/Shared/HttpRequestMessageExtensions.cs
@@ -1,38 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net.Http;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Travel.Api.Shared
-{
-	public static class HttpRequestMessageExtensions
-	{
-		private const string HttpContext = "MS_HttpContext";
-		private const string RemoteEndpointMessage = "System.ServiceModel.Channels.RemoteEndpointMessageProperty";
+//namespace Travel.Api.Shared
+//{
+//	public static class HttpRequestMessageExtensions
+//	{
+//		private const string HttpContext = "MS_HttpContext";
+//		private const string RemoteEndpointMessage = "System.ServiceModel.Channels.RemoteEndpointMessageProperty";
 
-		public static string GetClientIpAddress(this HttpRequestMessage request)
-		{
-			if (request.Properties.ContainsKey(HttpContext))
-			{
-				dynamic ctx = request.Properties[HttpContext];
-				if (ctx != null)
-				{
-					return ctx.Request.UserHostAddress;
-				}
-			}
+//		public static string GetClientIpAddress(this HttpRequestMessage request)
+//		{
+//			if (request.Properties.ContainsKey(HttpContext))
+//			{
+//				dynamic ctx = request.Properties[HttpContext];
+//				if (ctx != null)
+//				{
+//					return ctx.Request.UserHostAddress;
+//				}
+//			}
 
-			if (request.Properties.ContainsKey(RemoteEndpointMessage))
-			{
-				dynamic remoteEndpoint = request.Properties[RemoteEndpointMessage];
-				if (remoteEndpoint != null)
-				{
-					return remoteEndpoint.Address;
-				}
-			}
+//			if (request.Properties.ContainsKey(RemoteEndpointMessage))
+//			{
+//				dynamic remoteEndpoint = request.Properties[RemoteEndpointMessage];
+//				if (remoteEndpoint != null)
+//				{
+//					return remoteEndpoint.Address;
+//				}
+//			}
 
-			return null;
-		}
-	}
-}
+//			return null;
+//		}
+//	}
+//}
