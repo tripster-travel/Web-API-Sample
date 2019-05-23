feb68a9f68ed76c636ecc968b73a5eed8b7c70b8
Fred Sobel <fred.sobel@gmail.com>
2019-05-22 21:49:40 -0500

    add test client

---
+++ Travel.WebClient/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Travel.WebClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public async Task<ActionResult> Index()
+        {
+			var model = new TravelModel();
+
+			var api = new TravelApi("https://travel-web-api.azurewebsites.net/", new System.Net.Http.HttpClient());
+
+			var response = await api.Products_ListAsync();
+
+			if (response != null)
+			{
+				model.Products = response.Products;
+			}
+
+			return View(model);
+        }
+    }
+}
---
+++ Travel.WebClient/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Travel.WebClient.MvcApplication" Language="C#" %>
